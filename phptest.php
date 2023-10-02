e21c66966f594f4a2e964b0600fecd21de02dd11
Hope Celestine <98664134+Celestinehope@users.noreply.github.com>
2023-10-02 21:19:13 +0300

    Add files via upload

---
+++ phptest.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>This is test run for the php file, group 10 ICSA</p>'; ?>
+    </body>
+</html>
